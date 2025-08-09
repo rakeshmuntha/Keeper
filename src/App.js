14fcaa661e28b95e6773b2403e093976eb447ae1
rakeshmuntha <rakeshmuntha27@gmail.com>
2025-08-09 13:52:38 +0530

    added one to hit the backend

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import NoteState from './context/notes/NoteState';
 import AppContent from './components/AppContent';
 
 function App() {
     const [alert, setalert] = useState(null);
+    const host = process.env.REACT_APP_API_URL;
+
 
     const showalert = (message, type) => {
         setalert({ msg: message, type: type });
@@ -11,6 +13,19 @@ function App() {
             setalert(null);
         }, 3000);
     };
+
+    const hitbackend = async () => {
+        fetch(`${host}`)
+            .then(async (res) => {
+                await res.text();
+        })
+    }
+
+    useEffect(() => {
+        hitbackend();
+        // eslint-disable-next-line
+    }, [])
+
 
     return (
         <NoteState>
